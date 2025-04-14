09fb906eff6b6968ac669145e37338bf30b0e9aa
lionheart14 <leon11.herz@gmail.com>
2025-04-14 13:20:28 +0200

    exercises(chapter3): finished exercises 3.9, 3.12

--- excercises/chapter_03/Exercises.java
+++ excercises/chapter_03/Exercises.java
@@ -6,7 +6,7 @@ public class Exercises {
     public static void main(String[] args) {
         Exercises exercises = new Exercises();
 
-        exercises.randomMonth();
+        exercises.palindromeChecker();
     }
 
     /**
@@ -66,9 +66,47 @@ public class Exercises {
         System.out.print("Enter the first 9 digits of an ISBN as integer: ");
         int ISBN9 = input.nextInt();
         
-        // divide number by digit to extract numbers
-        // int tenthDigit =
+        int d1 = ISBN9 / 100000000;
+        int d2 = ISBN9 / 10000000 % 10;
+        int d3 = ISBN9 / 1000000 % 10;
+        int d4 = ISBN9 / 100000 % 10;
+        int d5 = ISBN9 / 10000 % 10;
+        int d6 = ISBN9 / 1000 % 10;
+        int d7 = ISBN9 / 100 % 10;
+        int d8 = ISBN9 / 10 % 10;
+        int d9 = ISBN9 % 10;
+
+        int checksum = (d1 * 1 + d2 * 2 + d3 * 3 + d4 * 4 + d5 * 5 + d6 * 6 + d7 * 7 + d8 * 8 + d9 * 9) % 11;
+
+        System.out.print("The ISBN-10 number is " + ISBN9);
+
+        if(checksum == 10) {
+            System.out.println("X");
+        } else {
+            System.out.println(checksum);
+        }
     }
+
+    /**
+     * 3.12
+     */
+    public void palindromeChecker() {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a three-digit integer: ");
+        int number = input.nextInt();
+
+        int firstDigit = number / 100;
+        int secondDigit = number / 10 % 10;
+        int thirdDigit = number % 10;
+
+        if(firstDigit == thirdDigit) {
+            System.out.println(number + " is a palindrome");
+        } else {
+            System.out.println(number + " is not a palindrome");
+        }
+    }
+
+    
 
 
 
