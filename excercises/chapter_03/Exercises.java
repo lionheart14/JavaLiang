c01701de580f984789a7cfcc856ed068a2ca2236
lionheart14 <leon11.herz@gmail.com>
2025-04-18 13:42:12 +0200

    exercises(chapter3): finished 3.18

--- excercises/chapter_03/Exercises.java
+++ excercises/chapter_03/Exercises.java
@@ -6,7 +6,7 @@ public class Exercises {
     public static void main(String[] args) {
         Exercises exercises = new Exercises();
 
-        exercises.scissorRockPaper();
+        exercises.calculatingCostOfShipping();
     }
 
     /**
@@ -154,6 +154,33 @@ public class Exercises {
         } else {
             String temp = number == 1 ? "rock" : "paper";
             System.out.println("Win, PC has " + temp);
+        }
+    }
+
+    /**
+     * 3.18
+     */
+    public void calculatingCostOfShipping() {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter weight of package: ");
+        double weight = input.nextDouble();
+        double cost = 0;
+
+        if(weight <= 2.5) {
+            cost = 2.5;
+        } else if(weight <= 4) {
+            cost = 4.5;
+        } else if(weight <= 10) {
+            cost = 7.5;
+        } else if(weight <= 20) {
+            cost = 10.5;
+        }
+
+        if(weight > 20) {
+            System.out.println("The package cannot be shipped.");
+        } else {
+            System.out.println("The cost of package is " + cost);
         }
     }
 }
