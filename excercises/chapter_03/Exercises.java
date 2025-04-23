30126dbe9a513a810de3173c2dd58220f33ec19c
Lionheart14 <leon01.herz@gmail.com>
2025-04-23 12:06:32 +0200

    exercises(chapter3): 3.23, 3.24

--- excercises/chapter_03/Exercises.java
+++ excercises/chapter_03/Exercises.java
@@ -6,7 +6,7 @@ public class Exercises {
     public static void main(String[] args) {
         Exercises exercises = new Exercises();
 
-        exercises.pointInCircle();
+        exercises.pickACard();
     }
 
     /**
@@ -247,11 +247,68 @@ public class Exercises {
         double p1 = input.nextDouble();
         double p2 = input.nextDouble();
 
-        double distance = Math.pow((Math.pow(p2 - p1, 2)) - Math.pow(p2 - p1, 2), 0.5);
+        double distance = Math.pow((Math.pow(p1 - p1, 2)) - Math.pow(p2 - p1, 2), 0.5);
+
         if(distance > 10) {
-            System.out.println("Point is not in the circle");
+            System.out.println("Point " + "(" + p1 + ", " + p2 + ") is not in the rectangle");
         } else {
-            System.out.println("Point is in the circle");
+            System.out.println("Point " + "(" + p1 + ", " + p2 + ") is in the rectangle");
         }
+    }
+
+    /**
+     * 3.23
+     */
+    public void pointInRectangle() {
+        Scanner input = new Scanner(System.in);
+        System.out.print ("Enter a point with two coordinates: ");
+        double p1 = input.nextDouble();
+        double p2 = input.nextDouble();
+
+        double horizontalDistance = 10 / 2;
+        double verticalDistance = 5.0 / 2;
+
+        if(p1 <= horizontalDistance && p2 <= verticalDistance) {
+            System.out.println("Point " + "(" + p1 + ", " + p2 + ") is in the rectangle");
+        } else {
+            System.out.println("Point " + "(" + p1 + ", " + p2 + ") is not in the rectangle");
+
+        }
+    }
+
+    /**
+     * 3.24
+     */
+    public void pickACard() {
+        int rankNum = (int) (Math.random() * 13 + 1);
+        int suitNum = (int) (Math.random() * 4 + 1);
+
+        String rank = "error";
+        String suit = "error";
+
+        switch(rankNum) {
+            case 1: rank = "Ace"; break;
+            case 2: rank = "2"; break;
+            case 3: rank = "3"; break;
+            case 4: rank = "4"; break;
+            case 5: rank = "5"; break;
+            case 6: rank = "6"; break;
+            case 7: rank = "7"; break;
+            case 8: rank = "8"; break;
+            case 9: rank = "9"; break;
+            case 10: rank = "10"; break;
+            case 11: rank = "Jack"; break;
+            case 12: rank = "Queen"; break;
+            case 13: rank = "King"; break;
+        }
+
+        switch(suitNum) {
+            case 1: suit = "Clubs"; break;
+            case 2: suit = "Diamonds"; break;
+            case 3: suit = "Hearts"; break;
+            case 4: suit = "Spades"; break;
+        }
+        
+        System.out.println("The card you picked is " + rank + " of " + suit);
     }
 }
