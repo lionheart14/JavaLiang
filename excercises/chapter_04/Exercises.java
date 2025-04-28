87c126766017f88ccc4aa6ea3a01007971faeaee
lionheart14 <leon11.herz@gmail.com>
2025-04-28 11:07:07 +0200

    exercises(chapter4: added method greatCircleDistance

--- excercises/chapter_04/Exercises.java
+++ excercises/chapter_04/Exercises.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Exercises {
     public static void main(String[] args) {
         Exercises exercises = new Exercises();
-        exercises.areaOfPentagon();
+        exercises.greatCircleDistance();
     }
     /** 
      * 4.1
@@ -21,5 +21,22 @@ public class Exercises {
         System.out.println("The are of the pentagon is " + area);
     }
 
-    
+    /**
+     * 4.2
+     */
+    public void greatCircleDistance() {
+        Scanner input = new Scanner(System.in);
+        final double RADIUS = 6371.01;
+
+        System.out.print("Enter point 1 (latitude and longitude) in degrees: ");
+        double x1 = Math.toRadians(input.nextDouble());
+        double y1 = Math.toRadians(input.nextDouble());
+
+        System.out.print("Enter point 2 (latitude and longitude) in degrees: ");
+        double x2 = Math.toRadians(input.nextDouble());
+        double y2 = Math.toRadians(input.nextDouble());
+
+        double d = RADIUS * Math.acos(Math.sin(x1) * Math.sin(x2) + Math.cos(x1) * Math.cos(x2) * Math.cos(y1 - y2));
+        System.out.printf("The distance between the two points is %.2f km", d);
+    }
 }
