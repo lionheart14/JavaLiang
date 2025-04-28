f979d21892e2062b5328e59edbf33724727183f8
lionheart14 <leon11.herz@gmail.com>
2025-04-28 12:28:18 +0200

    added method areaOfPolygon

--- excercises/chapter_04/Exercises.java
+++ excercises/chapter_04/Exercises.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Exercises {
     public static void main(String[] args) {
         Exercises exercises = new Exercises();
-        exercises.greatCircleDistance();
+        exercises.areaOfPolygon();
     }
     /** 
      * 4.1
@@ -39,4 +39,23 @@ public class Exercises {
         double d = RADIUS * Math.acos(Math.sin(x1) * Math.sin(x2) + Math.cos(x1) * Math.cos(x2) * Math.cos(y1 - y2));
         System.out.printf("The distance between the two points is %.2f km", d);
     }
+
+    /**
+     * 4.5
+     */
+    public void areaOfPolygon() {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the number of sides: ");
+        int n = input.nextInt();
+
+        System.out.print("Enter the side: ");
+        double s = input.nextDouble();
+
+        double area = (n * Math.pow(s, 2)) / (4 * Math.tan(Math.PI / n));
+
+        System.out.printf("The area of the polygon is %.2f", area);
+    }
+
+    
 }
