244625e4b1962bcdf389e906d4b395ab09d796f4
Lionheart14 <leon01.herz@gmail.com>
2025-05-06 11:03:21 +0200

    started method 5.1 passOrFail()

---
+++ excercises/chapter_05/Exercises.java
@@ -0,0 +1,18 @@
+package excercises.chapter_05;
+
+import java.util.Scanner;
+
+public class Exercises {
+    public static void main(String[] args) {
+        Exercises exercises = new Exercises();
+    }
+
+    /**
+     * 5.1
+     */
+    public void passOrFail() {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your score: ");
+        
+    }
+}
