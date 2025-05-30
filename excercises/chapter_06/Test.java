fa1f3c2c6655aa2d869980457986f715aaa62f64
lionheart14 <leon01.herz@gmail.com>
2025-05-30 16:22:43 +0200

    chapter6 added 6.4.8

--- excercises/chapter_06/Test.java
+++ excercises/chapter_06/Test.java
@@ -4,4 +4,35 @@ public class Test {
     public static void main(String[] args) {
         
     }
+
+    /**
+     * 6.48
+     */
+    public int salesComission(int salesAmount, int comission) {
+        return 0;
+    }
+
+    public String calendar(int month, int year) {
+        return "";
+    }
+
+    public double squareRoot(int number) {
+        return 0.0;
+    }
+
+    public boolean isEven(int number) {
+        return false;
+    }
+
+    public void displayMessage(int times) {
+        System.out.println("");
+    }
+
+    public int monthlyPayment(int loanAmount, int years, double annualInterestRate) {
+        return 0;
+    }
+
+    public char convertLetter(char c) {
+        return ' ';
+    }
 }
