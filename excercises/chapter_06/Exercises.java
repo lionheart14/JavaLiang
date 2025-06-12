4eb2557bbcdb0878308db8f0fe8ed3252de11c79
lionheart14 <leon01.herz@gmail.com>
2025-06-12 21:20:55 +0200

    exercises6 added getPentagonalNumber()

---
+++ excercises/chapter_06/Exercises.java
@@ -0,0 +1,15 @@
+package excercises.chapter_06;
+
+public class Exercises {
+    public static void main(String[] args) {
+        
+    }
+
+    /**
+     * 6.1
+     */
+    public static int getPentagonalNumbers(int n) {
+        int pentagonalNumber = n * (3 * n - 1) / 2;
+        return pentagonalNumber;
+    }
+}
