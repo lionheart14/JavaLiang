2a2f52f3b8b16d8fab2910056ee259709066f9e3
lionheart14 <leon01.herz@gmail.com>
2025-07-12 15:48:10 +0200

    exercises8 finished 8.2

--- excercises/chapter_08/Exercises.java
+++ excercises/chapter_08/Exercises.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Exercises {
     public static void main(String[] args) {
-        testSumRow();
+        testAverageMajorDiagonal();
     }
     /**
      * 8.1
@@ -30,5 +30,31 @@ public class Exercises {
         for(int row = 0; row < matrix.length; row++) {
             System.out.println("Sum of the elemets at row " + row + " is " + sumRow(matrix, row));
         }
+    }
+
+    /**
+     * 8.2
+     */
+    public static double averageMajorDiagonal(double[][] m) {
+        double average = 0;
+        int diagonalIndex = 0;
+        for(int row = 0; row < m.length; row++) {
+                average += m[row][diagonalIndex];
+                diagonalIndex++;
+        }
+        average /= m.length;
+        return average;
+    }
+
+    public static void testAverageMajorDiagonal() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a 4-by-4 matrix row by row: ");
+        double[][] matrix = new double[4][4];
+        for(int row = 0; row < matrix.length; row++) {
+            for(int column = 0; column < matrix[row].length; column++) {
+                matrix[row][column] = input.nextDouble();
+            }
+        }
+        System.out.println("Average of the elements in the major diagonal is " + averageMajorDiagonal(matrix));
     }
 }
