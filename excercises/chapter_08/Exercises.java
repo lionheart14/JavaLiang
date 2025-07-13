b6362fb8c1acdca5a82b081c9ca600063f57d144
lionheart14 <leon01.herz@gmail.com>
2025-07-13 12:36:54 +0200

    startet 8.4

--- excercises/chapter_08/Exercises.java
+++ excercises/chapter_08/Exercises.java
@@ -4,13 +4,21 @@ import java.util.Scanner;
 
 public class Exercises {
     public static void main(String[] args) {
-        testAverageMajorDiagonal();
+        displayEmployees();
     }
     /**
      * 8.1
      */
     public static double sumRow(double[][] m, int rowIndex) {
         double sum = 0;
+        for(int i = 0; i < m[rowIndex].length; i++) {
+            sum += m[rowIndex][i];
+        }
+        return sum;
+    }
+
+    public static int sumRow(int[][] m, int rowIndex) {
+        int sum = 0;
         for(int i = 0; i < m[rowIndex].length; i++) {
             sum += m[rowIndex][i];
         }
@@ -56,5 +64,46 @@ public class Exercises {
             }
         }
         System.out.println("Average of the elements in the major diagonal is " + averageMajorDiagonal(matrix));
+    }
+
+    /**
+     * 8.4
+     */
+    public static void displayEmployees() {
+        int[][] employees = {
+            {2, 4, 3, 4, 5, 8, 8},
+            {7, 3, 4, 3, 3, 4, 4},
+            {3, 3, 4, 3, 3, 2, 2},
+            {9, 3, 4, 7, 3, 4, 1},
+            {3, 5, 4, 3, 6, 3, 8},
+            {3, 4, 4, 6, 3, 4, 4},
+            {3, 7, 4, 8, 3, 8, 4},
+            {6, 3, 5, 9, 2, 7, 9}
+        };
+
+        int[] totalHours = new int[8];
+        for(int i = 0; i < employees.length; i++) {
+            totalHours[i] = sumRow(employees, i);
+        }
+
+        //TODO: too complicated, find easier solutions
+        for(int i = 0; i < employees.length; i++) {
+            int n = findSmallestInt(totalHours);
+            System.out.println("Employee " + i + " total working hours: " + sum);
+        }
+
+    }
+
+    public static int findSmallestInt(int[] list) {
+        int smallestNumber = list[0];
+        int index = 0;
+        for(int i = 0; i < list.length; i++) {
+            if(list[i] < smallestNumber) {
+                smallestNumber = i;
+                index = i;
+            }
+        }
+        list[index] = 99;
+        return smallestNumber;
     }
 }
