dbc04c6705a5f5a51c2bc8a42b4ae280d76d44d7
lionheart14 <leon01.herz@gmail.com>
2025-07-31 14:17:54 +0200

    exercises9 finished 9.6

---
+++ excercises/chapter_09/StopWatch.java
@@ -0,0 +1,16 @@
+package excercises.chapter_09;
+
+import java.util.Date;
+
+public class StopWatch {
+    private Date startTime;
+    private Date endTime;
+
+    public StopWatch() {
+
+    }
+
+    public start() {
+        startTime = 
+    }
+}
