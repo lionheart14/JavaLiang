14fac877b03c91f7fbefa2e475232139596bff76
lionheart14 <leon01.herz@gmail.com>
2025-08-08 14:37:06 +0200

    exercsies10 finished 10.16

--- excercises/chapter_10/Exercises.java
+++ excercises/chapter_10/Exercises.java
@@ -1,12 +1,13 @@
 package excercises.chapter_10;
 
+import java.math.BigInteger;
 import java.util.Stack;
 
 import excercises.chapter_10.MyInteger;
 
 public class Exercises {
     public static void main(String[] args) {
-        displayPrimeNumbers();
+        divisibleBy2Or3();
     }
 
     public static void displayPrimeNumbers() {
@@ -19,6 +20,23 @@ public class Exercises {
 
         for (Integer integer : stack) {
             System.out.println(integer);
+        }
+    }
+
+    public static void divisibleBy2Or3() {
+        BigInteger num = new BigInteger("50000000000000000000000000000000000000000000000000");
+        final BigInteger ZERO = new BigInteger("0");
+        final BigInteger ONE = new BigInteger("1");
+        final BigInteger TWO = new BigInteger("2");
+        final BigInteger THREE = new BigInteger("3");
+        int numberOfDivisibles = 0;
+
+        while(numberOfDivisibles < 50) {
+            if(num.mod(TWO).equals(ZERO) || num.mod(THREE).equals(ZERO)) {
+                System.out.println(num);
+                numberOfDivisibles++;
+            }
+            num = num.add(ONE);
         }
     }
 }
