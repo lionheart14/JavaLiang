6abadd11ed12a1a18ecdcea73e90bff9a70c5f11
lionheart14 <leon01.herz@gmail.com>
2025-09-17 11:02:53 +0200

    feat: Implement solution for exercise 11.13

--- excercises/chapter_11/Exercises.java
+++ excercises/chapter_11/Exercises.java
@@ -4,13 +4,14 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.Scanner;
 
 import excercises.chapter_10.Circle2D;
 
 public class Exercises {
     public static void main(String[] args) {
-        toStringShuffle();
+        removeDuplicateTest();
     }
 
     public static Integer maximumElementInArrayList(ArrayList<Integer> list) {
@@ -73,5 +74,35 @@ public class Exercises {
             sum += double1;
         }
         return sum;
+    }
+
+    public static void removeDuplicateTest() {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter 10 integers: ");
+        ArrayList<Integer> numbers = new ArrayList<>();
+        for(int i = 0; i < 10; i++) {
+            numbers.add(input.nextInt());
+        }
+
+        removeDuplicate(numbers);
+
+        System.out.print("The distinct integers are ");
+        Iterator iterator = numbers.iterator();
+        while(iterator.hasNext()) {
+            System.out.print(iterator.next() + " ");
+        }
+    }
+
+    public static void removeDuplicate(ArrayList<Integer> list) {
+        ArrayList<Integer> distinct = new ArrayList<>();
+        for (int i = 0; i < list.size(); i++) {
+            int element = list.get(i);
+            if(distinct.contains(element)) {
+                list.remove(i);
+                i--;
+            } else {
+                distinct.add(element);
+            }
+        }
     }
 }
