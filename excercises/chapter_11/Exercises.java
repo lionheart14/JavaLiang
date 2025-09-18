7c8041aca739269cce312de65ed94e6cb5a9eb49
lionheart14 <leon01.herz@gmail.com>
2025-09-18 12:42:40 +0200

    feat: Implement solution for exercise 11.19

--- excercises/chapter_11/Exercises.java
+++ excercises/chapter_11/Exercises.java
@@ -12,7 +12,7 @@ import excercises.chapter_10.Circle2D;
 
 public class Exercises {
     public static void main(String[] args) {
-        System.out.println(toCharacterArray("abc"));
+        binPacking();
     }
 
     public static Integer maximumElementInArrayList(ArrayList<Integer> list) {
@@ -190,5 +190,40 @@ public class Exercises {
             array.add(s.charAt(i));
         }
         return array;
+    }
+
+    public static void binPacking() {
+        final int CONTAINER_SIZE = 10;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the number of objects: ");
+        int num = input.nextInt();
+
+        System.out.print("Enter the weights of the objects: ");
+        ArrayList<Integer> objects = new ArrayList<>();
+        for(int i = 0; i < num; i++) {
+            objects.add(input.nextInt());
+        }
+
+        Iterator iterator = objects.iterator();
+        ArrayList<Integer> container = new ArrayList<>();
+        int containerNum = 1;
+        int currentSize = 0;
+        while(iterator.hasNext()) {
+            int element = (int) iterator.next();
+            if(currentSize + element < CONTAINER_SIZE) {
+                currentSize += element;
+                container.add(element);
+            } else {
+                System.out.print("Container " + containerNum + " contains objects with weight ");
+                for (Integer integer : container) {
+                    System.out.print(integer + " ");
+                }
+                container.clear();
+                container.add(element);
+                currentSize = element;
+                containerNum++;
+                System.out.println();
+            }
+        }
     }
 }
