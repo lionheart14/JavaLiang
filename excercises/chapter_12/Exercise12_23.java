3cac0fec6f1e9253d25e423db26894d20f2b91c4
Lionheart14 <leon01.herz@gmail.com>
2025-09-26 22:17:35 +0200

    note: added todo exercise 12.23

---
+++ excercises/chapter_12/Exercise12_23.java
@@ -0,0 +1,16 @@
+package excercises.chapter_12;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Exercise12_23 {
+    public static void main(String[] args) {
+        Path webPath = Paths.get("http://liveexample.pearsoncmg.com/data/Scores.txt");
+        //TODO 
+        try {
+            
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+    }
+}
