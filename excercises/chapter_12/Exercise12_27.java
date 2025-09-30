d3393ea3c9f1072680e303bcaf68c01a5a82173f
lionheart14 <leon01.herz@gmail.com>
2025-09-30 13:13:52 +0200

    feat: Implement solution for exercise 12.27

--- excercises/chapter_12/Exercise12_27.java
+++ excercises/chapter_12/Exercise12_27.java
@@ -1,9 +1,12 @@
 package excercises.chapter_12;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
 public class Exercise12_27 {
@@ -14,24 +17,41 @@ public class Exercise12_27 {
         input.close();
 
         Path fullPath = Paths.get("src/exercises", goalDirectory);
+        Pattern pattern = Pattern.compile("Exercise(\\d+)_(\\d+).java");
 
         try (Stream<Path> stream = Files.walk(fullPath)) {
             stream
                 .filter(path -> Files.isRegularFile(path))
                 .forEach(file -> {
+
+
                     String fileName = file.getFileName().toString();
+                    Matcher matcher = pattern.matcher(fileName);
 
-                    if(fileName.startsWith("Exercise")) {
-                        String numbers = 
+                    while(matcher.matches()) {
+                        try {
+                            String i = matcher.group(1);
+                            String j = matcher.group(2);
+    
+                            if(i.length() == 1) {
+                                i = "0" + i;
+                            }
+    
+                            if(j.length() == 1) {
+                                j = "0" + j;
+                            }
+    
+                            String newFileName = "Exercise" + i + "_" + j + ".java";
+                            Path newPath = file.resolveSibling(newFileName);
+                            Files.move(file, newPath);    
+                        } catch (IOException e) {
+                           System.out.println("Error renaming file");
+                        }
                     }
-
-
                 }
-                )
-            
-        } catch (Exception e) {
-            // TODO: handle exception
+            );
+        } catch (IOException e) {
+            System.out.println("Error reading file");
         }
-
     }
 }
