57386755da2ca92f53379ee26f4d9d4e4c289d91
Lionheart14 <leon01.herz@gmail.com>
2025-10-27 11:57:47 +0100

    feat: Implement solution for exercise 13.13

---
+++ excercises/chapter_13/Course.java
@@ -0,0 +1,42 @@
+package excercises.chapter_13;
+
+public class Course implements Cloneable{
+
+    private String courseName;
+    private String[] students = new String[100];
+    private int numberOfStudents;
+
+    public Course(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public void addStudent(String student) {
+        students[numberOfStudents] = student;
+        numberOfStudents++;
+    }
+
+    public String[] getStudents() {
+        return students;
+    }
+
+    public int getNumberOfStudents() {
+        return numberOfStudents;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void dropStudent(String student) {
+        // Left as an exercise in Programming Exercise 10.9
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        Course newCourse = (Course) super.clone();
+
+        newCourse.students = students.clone();
+        
+        return newCourse;
+    }
+}
