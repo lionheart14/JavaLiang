4963a97ea2a1e54d8077faac44b7ba8f1f3a49b1
Lionheart14 <leon01.herz@gmail.com>
2025-11-09 17:31:37 +0100

    feat: Added animation logic to exercise 15.29

--- src/excercises/chapter_15/liang/Exercise15_29.java
+++ src/excercises/chapter_15/liang/Exercise15_29.java
@@ -1,5 +1,7 @@
 package excercises.chapter_15.liang;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -11,8 +13,10 @@ import javafx.scene.shape.Circle;
 import javafx.scene.shape.Polygon;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 public class Exercise15_29 extends Application {
+    private int animationSpeed = 1;
 
     @Override
     public void start(Stage arg0) throws Exception {
@@ -71,10 +75,16 @@ public class Exercise15_29 extends Application {
                 case LEFT:
                     carPane.setLayoutX(carPane.getLayoutX() - 20);
                     break;
+                case UP:
+                    animationSpeed++;
+                    break;
+                case DOWN:
+                    animationSpeed--;
+                    break;
                 default:
                     break;
             }
-            
+
             if(carPane.getLayoutX() > 500) {
                 carPane.setLayoutX(0);
             } else if(carPane.getLayoutX() < 0) {
@@ -82,11 +92,30 @@ public class Exercise15_29 extends Application {
             }
         });
 
+        //Animation
+        Timeline animation = new Timeline();
+
+        KeyFrame frame = new KeyFrame(
+            Duration.millis(16),
+            event -> {
+                carPane.setLayoutX(carPane.getLayoutX() + animationSpeed);
+
+                if(carPane.getLayoutX() > 500) {
+                    carPane.setLayoutX(0);
+                }
+            }
+        );
+
+        animation.getKeyFrames().add(frame);
+        animation.setCycleCount(Timeline.INDEFINITE);
+        animation.play();
+
+        
         root.getChildren().add(carPane);
 
         Scene scene = new Scene(root, 500, 500);
 
-        arg0.setTitle("15.29 (mit PANE)");
+        arg0.setTitle("15.29");
         arg0.setScene(scene);
         arg0.show();
 
